fc81c24b8e30c33196fb91de22f6350dbc91a990
Steve Hsu <stevehsu@stevede-macbook-pro.local>
2018-12-22 12:43:14 +0800

    Initial Commit

---
+++ version control test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  version control test
+//
+//  Created by Steve Hsu on 2018/12/22.
+//  Copyright © 2018 Steve Hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ version control testTests/version_control_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  version_control_testTests.swift
+//  version control testTests
+//
+//  Created by Steve Hsu on 2018/12/22.
+//  Copyright © 2018 Steve Hsu. All rights reserved.
+//
+
+import XCTest
+@testable import version_control_test
+
+class version_control_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
