efa5761d3d137e60e1a68c26ec4864d8bb5e1a01
Steve Hsu <stevehsu@stevede-macbook-pro.local>
2018-12-22 14:47:29 +0800

    Add version2

--- version control test/ViewController.swift
+++ version control test/ViewController.swift
@@ -15,5 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    func version2 (){
+        
+    }
+    
 }
 
