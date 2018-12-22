d3f49b4aa50f73ac864ba4e201312dfd0bdac0f2
Steve Hsu <stevehsu@stevede-macbook-pro.local>
2018-12-22 13:53:07 +0800

    Delete two functions the something and version1

--- version control test/ViewController.swift
+++ version control test/ViewController.swift
@@ -14,14 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    func someting (){
-        
-    }
-    
-    func version1 (){
-        
-    }
     
 }
 
